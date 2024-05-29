22f0bcaef4c0028f5f2efdff3778af8be51e1a26
nsquansah <ninaspioquansah@gmail.com>
2024-05-29 23:23:11 +0000

    working on sidebar list

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import '../App.css';
 import styles from './style.module.css';
+import { SidebarData } from './SidebarData';
 
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <div className={styles.sidebarheader}>
         <div>
-          <h1 className={styles.symbol}> N</h1> 
+          <h1 className={styles.symbol}>N</h1>
           <h2 className={styles.name}>Nina</h2>
           <h3 className={styles.title}>Web Developer</h3>
         </div>
-        <div>
-          
-          </div>
-        
       </div>
-      {/* Add more content for your sidebar here */}
+      <ul className={styles.sidebarlist}>
+        {SidebarData.map((val, key) => (
+          <li
+            key={key}
+            className={styles.row}
+            id={window.location.pathname === val.path ? "active" : ""}
+            onClick={() => {
+              window.location.pathname = val.path;
+            }}
+          >
+            <div id="title">{val.title}</div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default Sidebar; // Use default export
+export default Sidebar;
---
+++ src/components/SidebarData.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export const SidebarData = [
+
+    {
+        title: "About",
+        path: "/about",
+        
+    },
+    {
+        title: "My Skills",
+        path: "/skills",
+        
+    },
+    {
+        title: "Projects",
+        path: "/projects",
+        
+    },
+    {
+        title: "Contact",
+        path: "/contact",
+        
+    },
+    {
+        title: "Resume",
+        path: "/resume",
+        
+    },
+    {
+        title: "Blog",
+        path: "/blog",
+    },
+    ];
