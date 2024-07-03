f2a73292834ea6f36141367aed8f83056ced2a19
nsquansah <ninaspioquansah@gmail.com>
2024-07-03 13:08:35 +0000

    home page and sidebar is responsive

--- src/pages/AboutPage/index.jsx
+++ src/pages/AboutPage/index.jsx
@@ -1,12 +1,13 @@
 // src/pages/AboutPage/index.jsx
 import React from 'react';
+import Style from './style.module.css';
 
 export const AboutPage = () => {
   return (
-    <div>
-      <h1>About Me</h1>
-      <p>This is the About pagetttttttttttttttttttttttttttttttttttttttttttttttttt.</p>
-    </div>
+    <div className={Style.AboutPage}>
+    
+
+      </div>
   );
 };
 
